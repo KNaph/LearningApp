6df5da39cf699a2ffdc4fe2e6cacb1b34f6efb36
Kyle Phan <kylephan@tecace.com>
2018-03-09 18:08:26 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kylephan/learningapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kylephan.learningapp;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        InputFragment inFrag = new InputFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, inFrag);
+        fragmentTransaction.commit();
+
+    }
+}
